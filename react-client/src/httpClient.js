84ae879ac1b130c789457298cf2e30ebdfb2463d
negotiate-merge <nigel@synergitech.com.au>
2025-05-29 16:17:44 +1000

    Change pump svg icon. Force redirect to / when 401 recieved.

--- react-client/src/httpClient.js
+++ react-client/src/httpClient.js
@@ -1,6 +1,23 @@
 import axios from "axios";
+import { useNavigate } from 'react-router-dom';
 
-export default axios.create({
+const api = axios.create({
   baseURL: '/api',  // 'http://34.129.37.135:5000',
   withCredentials: true,
 });
+
+// Redirect to home if 401 received
+api.interceptors.response.use(
+  response => response,
+  error => {
+    if (error.response?.status === 401) {
+      const currentPath = window.location.pathname;
+      if (currentPath !== "/") {
+        window.location.href = '/';
+      }
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default api;
