8c7ef5ec981d7b802bba6f10fa32b860960a92c7
negotiate-merge <nigel@synergitech.com.au>
2025-06-27 06:14:49 +1000

    Prepare to bring in mqtt listener code, setup server.py for debug via config file - manipulated via flask_debug_mode to be added in next commit.

--- flask-server/server.py
+++ flask-server/server.py
@@ -176,4 +176,5 @@ def logout():
   
 
 if __name__ == "__main__":
-  app.run(host="127.0.0.1", port=5000)#, debug=True) # debug=True
+  app.run(host="127.0.0.1", port=5000, debug=app.config['DEBUG']) # debug=True
+  
